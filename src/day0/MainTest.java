dd1743bd9616603792fb659431b4d1ba688e24a9
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-07 12:50:36 +0100

    Add day0 for a template for every day

---
+++ src/day0/MainTest.java
@@ -0,0 +1,44 @@
+package day0;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static day0.Main.solve;
+import static day0.Main.solve2;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    String sampleInput;
+    String input;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        sampleInput = Files.readString(Path.of(getInputDir() + "/sample.txt"));
+        input = Files.readString(Path.of(getInputDir() + "/input.txt"));
+    }
+
+    private String getInputDir() {
+        return "src/" + getClass().getPackageName();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void solvePart1() {
+        assertEquals("TODO", solve(sampleInput));
+        System.out.println(solve(input));
+    }
+
+    @Test
+    void solvePart2() {
+        assertEquals("TODO", solve2(sampleInput));
+        System.out.println(solve2(input));
+    }
+}
---
+++ src/day0/Runner.java
@@ -0,0 +1,22 @@
+package day0;
+
+public class Runner {
+    private final long time;
+    private final long distance;
+
+    public Runner(long time, long distance) {
+        this.time = time;
+        this.distance = distance;
+    }
+
+    long getNbWinners() {
+        long startLosers = 0;
+        for (long i = 1; i < this.time; i++) {
+            if (i * (this.time - i) > this.distance) {
+                break;
+            } else {
+                startLosers++;
+            }
+        }
+        return this.time - 2 * startLosers - 1;
+    }}
