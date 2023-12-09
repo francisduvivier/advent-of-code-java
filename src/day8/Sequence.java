de47f9b0fbbda5b5651b29e8cfb58792340528ef
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-09 07:58:09 +0100

    Implement getDiffs and allSameDiff

--- src/day8/Sequence.java
+++ src/day8/Sequence.java
@@ -2,15 +2,39 @@ package day8;
 
 public class Sequence {
     private final long[] start;
+    private Sequence diffs;
 
 
     public Sequence(long[] start) {
         this.start = start;
+        long[] diffs = getDiffs();
+        if (!allSameDiffs(diffs)) {
+            this.diffs = new Sequence(diffs);
+        } else {
+            this.diffs = null;
+        }
+    }
+
+    private static boolean allSameDiffs(long[] diffs) {
+        for (var diff : diffs) {
+            if (diff != diffs[0]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean allSameDiff() {
-        // TODO
-        return false;
+        var diffs = this.getDiffs();
+        return allSameDiffs(diffs);
+    }
+
+    long[] getDiffs() {
+        long[] result = new long[this.start.length - 1];
+        for (int i = 0; i < start.length - 1; i++) {
+            result[i] = start[i + 1] - start[i];
+        }
+        return result;
     }
 
     long getDiff() {
--- src/day8/SequenceTest.java
+++ src/day8/SequenceTest.java
@@ -2,16 +2,26 @@ package day8;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SequenceTest {
 
     @Test
+    void getDiffs() {
+        assertArrayEquals(new long[]{1, 1}, new Sequence(new long[]{1, 2, 3}).getDiffs());
+        assertArrayEquals(new long[]{2, 2, 2, 2, 2}, new Sequence(new long[]{-2, 0, 2, 4, 6, 8}).getDiffs());
+        assertArrayEquals(new long[]{-2, -2}, new Sequence(new long[]{-2, -4, -6}).getDiffs());
+        assertArrayEquals(new long[]{3, -1}, new Sequence(new long[]{1, 4, 3}).getDiffs());
+        assertArrayEquals(new long[]{2, 2, 2, 2, 4}, new Sequence(new long[]{-2, 0, 2, 4, 6, 10}).getDiffs());
+        assertArrayEquals(new long[]{2, -2, -4}, new Sequence(new long[]{-4, -2, -4, -8}).getDiffs());
+    }
+
+    @Test
     void sameDiffs() {
-        // Assert
         assertEquals(true, new Sequence(new long[]{1, 2, 3}).allSameDiff());
         assertEquals(true, new Sequence(new long[]{-2, 0, 2, 4, 6, 8}).allSameDiff());
-        assertEquals(true, new Sequence(new long[]{-2, -4, -8}).allSameDiff());
+        assertEquals(true, new Sequence(new long[]{-2, -4, -6}).allSameDiff());
         // Assert
         assertEquals(false, new Sequence(new long[]{1, 4, 3}).allSameDiff());
         assertEquals(false, new Sequence(new long[]{-2, 0, 2, 4, 6, 10}).allSameDiff());
