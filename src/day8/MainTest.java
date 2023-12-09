677ccd2f29f6e617fc1dcd85bbc1008c3cd5e775
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-09 08:32:48 +0100

    Start day8

---
+++ src/day8/MainTest.java
@@ -0,0 +1,47 @@
+package day8;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static day8.Main.solve;
+import static day8.Main.solve2;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    String sampleInput;
+    String sampleInput2;
+    String input;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        sampleInput = Files.readString(Path.of(getInputDir() + "/sample.txt"));
+        sampleInput2 = Files.readString(Path.of(getInputDir() + "/sample2.txt"));
+        input = Files.readString(Path.of(getInputDir() + "/input.txt"));
+    }
+
+    private String getInputDir() {
+        return "src/" + getClass().getPackageName();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void solvePart1() {
+        assertEquals("2", solve(sampleInput));
+        assertEquals("6", solve(sampleInput2));
+        assertEquals("PART 1 SOLUTION IS", solve(input));
+    }
+
+    @Test
+    void solvePart2() {
+        assertEquals("TODO", solve2(sampleInput));
+        assertEquals("PART 2 SOLUTION IS", solve2(input));
+    }
+}
---
+++ src/day8/RunnerTest.java
@@ -0,0 +1,19 @@
+package day8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RunnerTest {
+
+    @Test
+    void getNbWinners() {
+        // Arrange
+        // Act
+        var nbWinners = new Runner(7, 9).getNbWinners();
+        // Assert
+        assertEquals(4, new Runner(7, 9).getNbWinners());
+        assertEquals(8, new Runner(15, 40).getNbWinners());
+        assertEquals(9, new Runner(30, 200).getNbWinners());
+    }
+}
