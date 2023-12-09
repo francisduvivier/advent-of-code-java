ab76b8a2201463b012502ca5cda24eb6821a9102
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-09 09:02:49 +0100

    Make BinaryNode a private subclass of BinaryTree

--- src/day8/BinaryTree.java
+++ src/day8/BinaryTree.java
@@ -3,15 +3,17 @@ package day8;
 import java.util.List;
 
 public class BinaryTree {
+    final BinaryNode[] nodes;
     final String leftId;
     final String rightId;
     final String id;
 
-    public BinaryTree(String id, String leftId, String rightId) {
+    public BinaryTree(BinaryNode[] nodes) {
+        this.nodes = nodes;
+        this.id = nodes[0].id;
+        this.leftId = nodes[0].leftId;
+        this.rightId = nodes[0].rightId;
 
-        this.id = id;
-        this.leftId = leftId;
-        this.rightId = rightId;
     }
 
     public static BinaryTree parse(List<String> strings) {
@@ -19,10 +21,23 @@ public class BinaryTree {
         String id = firstParts[0];
         String leftId = firstParts[1];
         String rightId = firstParts[2];
-        return new BinaryTree(id, leftId, rightId); //TODO
+        return new BinaryTree(new BinaryNode[]{new BinaryNode(id, leftId, rightId)});
     }
 
     public long getStepsRec(String destinationId, String instructions) {
         return 0; //TODO
+    }
+
+    private static class BinaryNode {
+        final String leftId;
+        final String rightId;
+        final String id;
+
+        public BinaryNode(String id, String leftId, String rightId) {
+
+            this.id = id;
+            this.leftId = leftId;
+            this.rightId = rightId;
+        }
     }
 }
