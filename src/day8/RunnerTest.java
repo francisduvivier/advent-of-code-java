497f7032caea748358b95f0e70dfd6857322d9d6
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-09 09:40:45 +0100

    Add loop detection test

--- src/day8/RunnerTest.java
+++ src/day8/RunnerTest.java
@@ -5,18 +5,22 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.InvalidParameterException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RunnerTest {
 
     private String[] sampleInput;
     private String[] sampleInput2;
+    private String[] loopSample;
 
     {
         try {
             sampleInput = Files.readString(Path.of(getInputDir() + "/sample.txt")).split("\n");
             sampleInput2 = Files.readString(Path.of(getInputDir() + "/sample2.txt")).split("\n");
+            loopSample = Files.readString(Path.of(getInputDir() + "/loop-sample.txt")).split("\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -39,8 +43,10 @@ class RunnerTest {
     void stepsTo() {
         Runner fistSampleParsed = Runner.parse(sampleInput);
         Runner secondSampleParsed = Runner.parse(sampleInput2);
+        Runner loopSampleParsed = Runner.parse(loopSample);
 
         assertEquals(2, fistSampleParsed.stepsTo("ZZZ"));
         assertEquals(6, secondSampleParsed.stepsTo("ZZZ"));
+        assertThrows(InvalidParameterException.class, () -> loopSampleParsed.stepsTo("ZZZ"));
     }
 }
