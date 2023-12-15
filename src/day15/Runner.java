444ea4e0c78dab0c0c33c8c58167dd9384e2df79
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-15 22:44:45 +0100

    Start Day 15

---
+++ src/day15/Runner.java
@@ -0,0 +1,11 @@
+package day15;
+
+public class Runner {
+
+    public Runner() {
+    }
+
+    long run() {
+        return 0;
+    }
+}
---
+++ src/day15/RunnerTest.java
@@ -0,0 +1,16 @@
+package day15;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RunnerTest {
+
+    @Test
+    void getNbWinners() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals(0, new Runner().run());
+    }
+}
