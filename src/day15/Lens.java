5b9b09f80fa1fe5ea27fcc1cf2ba3eb6651b5f05
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-15 23:18:43 +0100

    Add Lens class

---
+++ src/day15/Lens.java
@@ -0,0 +1,11 @@
+package day15;
+
+public class Lens {
+    String label;
+    short focal;
+
+    Lens(String label, short focal) {
+        this.focal = focal;
+        this.label = label;
+    }
+}
