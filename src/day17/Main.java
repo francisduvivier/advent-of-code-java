7e9060269acc79ba3a4659023e63515e7732612f
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-17 06:51:24 +0100

    Start Day 17

---
+++ src/day17/Main.java
@@ -0,0 +1,23 @@
+package day17;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    static String solve(String sampleInput) {
+        String[] lines = sampleInput.split("\n");
+        long result = 0;
+        // TODO
+        return "" + result;
+    }
+
+    static String solve2(String sampleInput) {
+        String[] lines = sampleInput.split("\n");
+        long result = 0;
+        // TODO
+        return "" + result;
+    }
+
+}
---
+++ src/day17/Runner.java
@@ -0,0 +1,11 @@
+package day17;
+
+public class Runner {
+
+    Runner() {
+    }
+
+    long run() {
+        return 0;
+    }
+}
