31e788c67849ce93fbac524376944625876d57d3
Francis Duvivier <fduvivier.contractor@libertyglobal.com>
2023-12-25 23:22:22 +0100

    Add pseudo algo to day 19

--- src/day19/Main.java
+++ src/day19/Main.java
@@ -8,6 +8,37 @@ public class Main {
 
     static String solve(String sampleInput) {
         String[] lines = sampleInput.split("\n");
+        /**
+         *  Plan here:
+         *  - First parse lines:
+         *    - Xmas object list {sum(), getLetter(), map<String, Long>}
+         *    - HashMap from flow name to Rule list, flowMap
+         *
+         *    sum = 0
+         *    For loop through xMas to exec findFlowResultRec, returns false in case of Reject
+         *      if(result) sum+= xMas.sum()
+         *
+         *    findFlowResultRec(flowName):
+         *      rules = getFlow(flowName):
+     *          for rule in rules:
+         *          destination = rule
+         *          if(rule.equals("A")){
+         *              return true;
+         *          }
+         *          else if(rule.equals("R")){
+         *              return false;
+         *          }else if(match "[><]"){
+         *           letter, biggerOrSmaller (>=1, <=-1), value, destination
+         *           if(xMas.getLetter(letter)*biggerOrSmaller > biggerOrSmaller * value){
+         *               destination
+         *           }
+         *           // Best extract this into tested function checkBiggerSmallerThanRule(xmas, String rule)
+         *          }else{
+         *              return findFlowResultRec(rule)
+         *          }
+         *          // TO follow the open closed principle, each of these rules should be some class implementing the boolean exec(xMas, flowMap) interface
+         */
+
         long result = 0;
         // TODO
         return "" + result;
